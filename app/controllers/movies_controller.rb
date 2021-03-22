bcb157454970d1dbd554111d061b0e3d3abd2f59
Mateus Senne <mateus.senne@matilha.digital>
2021-03-22 20:55:35 -0300

    Add scrape route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,24 +13,8 @@ class MoviesController < ApplicationController
     render json: @movie
   end
 
-  # POST /movies
-  def create
-    @movie = Movie.new(movie_params)
-
-    if @movie.save
-      render json: @movie, status: :created, location: @movie
-    else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /movies/1
-  def update
-    if @movie.update(movie_params)
-      render json: @movie
-    else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
+  def scrape
+    
   end
 
   # DELETE /movies/1
