3aaf33328c86563a1b484a6a22d96629ea101348
Mateus Senne <mateus.senne@matilha.digital>
2021-03-22 21:02:59 -0300

    Add process and parse url methods to movies spider

--- app/models/movies_spider.rb
+++ app/models/movies_spider.rb
@@ -1,2 +1,16 @@
 class MoviesSpider < Kimurai::Base
+  @name = 'movies_spider'
+  @engine = :mechanize
+
+  def self.process(url)
+    @start_urls = [url]
+    self.crawl!
+  end
+
+  def parse(response, url:, data: {})
+    response.xpath("//div[@class='shop-srp-listings__listing-container']").each do |vehicle|
+      item = {}
+    end
+  end
+
 end
